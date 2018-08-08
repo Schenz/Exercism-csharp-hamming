80961d88c31138ce0c2982f507a9eb28f5a5e13a
Schenz <bschenz@gmail.com>
2018-08-08 16:12:42 -0400

    final solution

--- Hamming.cs
+++ Hamming.cs
@@ -4,6 +4,20 @@ public static class Hamming
 {
     public static int Distance(string firstStrand, string secondStrand)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (firstStrand.Length != secondStrand.Length) {
+            throw new ArgumentException();
+        }
+
+        var distance = 0;
+
+        for(var index1 = 0; index1 < firstStrand.Length; index1++)
+        {
+            if (firstStrand[index1].ToString() != secondStrand.Substring(index1, 1)) 
+            {
+                distance++;
+            }
+        }
+        
+        return distance;
     }
 }
